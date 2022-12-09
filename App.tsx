6dd2d551297e4c5a7bc5f76c6d4b52a25896911e
Benjamin Kostiuk <benkostiuk1@gmail.com>
2022-12-08 21:27:28 -0500

    Add ant component to test

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, WhiteSpace } from "@ant-design/react-native";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up Test.tsx to start working on your app!</Text>
+      <WhiteSpace />
+      <Button type={"primary"}>
+        <Text>Sample Button Text</Text>
+      </Button>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
