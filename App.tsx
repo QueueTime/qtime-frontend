15d81d058071124f7e86f2c910daec7da1d8ecc9
Ben Kostiuk <benkostiuk1@gmail.com>
2022-12-08 23:25:25 -0500

    [FEAT] Add ant design component library and fonts (#18)
    * Add ant-design packages and dependencies
    
    * Add ant component to test
    
    * Add ant-design packages and dependencies
    
    * Add ant component to test
    
    * Add WorkSans font
    
    * Create styled text component
    
    * Load WorkSans font to display
    
    * Add path aliasing
    
    * Refactor paths to use path aliasing
    
    * Remove tunnel
    
    * Add tunnel command for development

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, WhiteSpace } from "@ant-design/react-native";
 
-export default function App() {
+import { StyledText } from "@components/StyledText";
+
+const App = () => {
+  // Load fonts
+  const [fontsLoaded] = useFonts({
+    WorkSans: require("@assets/fonts/WorkSans.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.huge}>Default Font Family Sample Text</Text>
+      <StyledText style={styles.huge}>
+        WorkSans Font Family Sample Text
+      </StyledText>
+      <WhiteSpace />
+      <Button type={"primary"}>
+        <StyledText>Sample Button Text</StyledText>
+      </Button>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  huge: {
+    fontSize: 20,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
