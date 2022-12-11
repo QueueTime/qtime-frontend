01dc6b210c69b10e4b15c964239c2978852d05c5
Benjamin Kostiuk <benkostiuk1@gmail.com>
2022-12-10 22:02:08 -0500

    Add route constants

---
+++ src/constants/routes.tsx
@@ -0,0 +1,22 @@
+/* Route names */
+export const SIGN_UP = "SignUp";
+export const REFERRAL = "Referral";
+export const TERMS_OF_SERVICE = "TermsOfService";
+export const ONBOARDING = "Onboarding";
+
+export const WAIT_TIMES = "WaitTimes";
+// This is required for the wait time stack navigator
+// This route corresponds to the home for the wait times tab
+export const WAIT_TIMES_HOME = "WaitTimesHome";
+export const LOCATION_DETAILS = "LocationDetails";
+
+export const MAP = "Map";
+export const REWARDS = "Rewards";
+
+export const PROFILE = "Profile";
+// This is required for the profile stack navigator
+// This route corresponds to the home for the profile tab
+export const PROFILE_HOME = "ProfileHome";
+export const SUGGEST_POI = "SuggestPOI";
+export const NOTIFICATIONS = "Notifications";
+export const THEME = "Theme";
