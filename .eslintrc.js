f6b6f3eee4685dc489ef52455f3a0b158fed60f4
Benjamin Kostiuk <benkostiuk1@gmail.com>
2022-12-12 15:33:22 -0500

    Update linter rules to not require React import, since I don't think we need it

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ["@react-native-community", "eslint-config-prettier"],
+  rules: {
+    // suppress errors for missing 'import React' in files
+    "react/react-in-jsx-scope": "off",
+    // allow jsx syntax in js files (for next.js project)
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
+  },
 };
