a3882cf2d289068266a68da2628b2cca3400e350
Benjamin Kostiuk <benkostiuk1@gmail.com>
2022-12-13 21:08:31 -0500

    Add types for wait times navigator

--- src/navigators/WaitTimeStackNavigator.tsx
+++ src/navigators/WaitTimeStackNavigator.tsx
@@ -1,17 +1,32 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 import { WaitTimeScreen } from "@screens/WaitTimeScreen";
 import { LocationDetailsScreen } from "@screens/LocationDetailsScreen";
 import * as ROUTES from "@constants/routes";
 
 // Types of parameters that are passed for each screen
-type IProfileStackNavigatorParams = {
+type WaitTimeStackNavigatorParams = {
   [ROUTES.WAIT_TIMES_HOME]: undefined;
   [ROUTES.LOCATION_DETAILS]: { location: string };
 };
 
+// Prop types for each of the screen in the navigator
+// ... ({ navigator, route }: RouteNameScreenProps)
+// OR navigator: RouteNameScreenProps['navigator']
+//    route: RouteNameScreenProps['route']
+export type WaitTimeScreenProps = NativeStackScreenProps<
+  WaitTimeStackNavigatorParams,
+  typeof ROUTES.WAIT_TIMES_HOME
+>;
+
+export type LocationDetailsScreenProps = NativeStackScreenProps<
+  WaitTimeStackNavigatorParams,
+  typeof ROUTES.LOCATION_DETAILS
+>;
+
 // Stack component to use for navigation
-const Stack = createNativeStackNavigator<IProfileStackNavigatorParams>();
+const Stack = createNativeStackNavigator<WaitTimeStackNavigatorParams>();
 
 /**
  * Handles navigation for the wait times section of the app
