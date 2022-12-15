d71d632afc562bcc0b6b7a3d58fca4a5f0b85848
Benjamin Kostiuk <benkostiuk1@gmail.com>
2022-12-14 22:46:38 -0500

    Add theme constants

---
+++ src/constants/theme.tsx
@@ -0,0 +1,14 @@
+export const themes = {
+  light: {
+    name: "light",
+    foreground: "#000000",
+    background: "#ffffff",
+  },
+  dark: {
+    name: "dark",
+    foreground: "#ffffff",
+    background: "#222222",
+  },
+};
+
+export const STORAGE_KEY: string = "QTIME_THEME";
