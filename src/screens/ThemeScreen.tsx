317aa2b02207170993b6ec01cadaca83eb81b0e3
Benjamin Kostiuk <benkostiuk1@gmail.com>
2022-12-15 22:25:54 -0500

    Update theme screen for changing theme

--- src/screens/ThemeScreen.tsx
+++ src/screens/ThemeScreen.tsx
@@ -1,19 +1,40 @@
+import { useContext } from "react";
 import { Text, View, StyleSheet } from "react-native";
 
-export const ThemeScreen = () => (
-  <View style={styles.container}>
-    <Text style={styles.huge}>App Appearance Screen</Text>
-  </View>
-);
+import { Button, WhiteSpace } from "@ant-design/react-native";
 
-const styles = StyleSheet.create({
-  huge: {
-    fontSize: 20,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+import { ThemeContext } from "@contexts/theme";
+
+export const ThemeScreen = () => {
+  const { theme, changeTheme } = useContext(ThemeContext);
+
+  const styles = StyleSheet.create({
+    huge: {
+      fontSize: 20,
+      color: theme.foreground,
+    },
+    container: {
+      flex: 1,
+      backgroundColor: theme.background,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.huge}>App Appearance Screen</Text>
+      <Text style={styles.huge}>Theme: {theme.name}</Text>
+      <WhiteSpace />
+      <Button type={"primary"} onPress={() => changeTheme("light")}>
+        Set light theme
+      </Button>
+      <Button type={"primary"} onPress={() => changeTheme("dark")}>
+        Set dark theme
+      </Button>
+      <Button type={"primary"} onPress={() => changeTheme("system")}>
+        Set system theme
+      </Button>
+    </View>
+  );
+};
