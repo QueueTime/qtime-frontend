cdb704724aeb3d4014a84dfbd92e04247a9938bf
Ben Kostiuk <benkostiuk1@gmail.com>
2022-12-16 11:12:54 -0500

    [FIX] Typescript prop types for react navigation (#25)
    * Add prop types for react naviagte for profile routes
    
    * Add types for wait times navigator
    
    * Fix prop types
    
    * Fix typo in comments

--- src/navigators/ProfileStackNavigator.tsx
+++ src/navigators/ProfileStackNavigator.tsx
@@ -1,4 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 import { POISuggestionScreen } from "@screens/POISuggestionScreen";
 import * as ROUTES from "@constants/routes";
@@ -6,8 +7,40 @@ import { NotificationsScreen } from "@screens/NotificationScreen";
 import { ThemeScreen } from "@screens/ThemeScreen";
 import { ProfileScreen } from "@screens/ProfileScreen";
 
+// Types of parameters that are passed for each screen
+type ProfileStackNavigatorParams = {
+  [ROUTES.PROFILE_HOME]: undefined;
+  [ROUTES.SUGGEST_POI]: undefined;
+  [ROUTES.NOTIFICATIONS]: undefined;
+  [ROUTES.THEME]: undefined;
+};
+
+// Prop types for each of the screen in the navigator
+// ... ({ navigation, route }: RouteNameScreenProps)
+// OR navigator: RouteNameScreenProps['navigation']
+//    route: RouteNameScreenProps['route']
+export type ProfileScreenProps = NativeStackScreenProps<
+  ProfileStackNavigatorParams,
+  typeof ROUTES.PROFILE_HOME
+>;
+
+export type POISuggestionScreenProps = NativeStackScreenProps<
+  ProfileStackNavigatorParams,
+  typeof ROUTES.SUGGEST_POI
+>;
+
+export type NotificationScreenProps = NativeStackScreenProps<
+  ProfileStackNavigatorParams,
+  typeof ROUTES.NOTIFICATIONS
+>;
+
+export type ThemeScreenProps = NativeStackScreenProps<
+  ProfileStackNavigatorParams,
+  typeof ROUTES.THEME
+>;
+
 // Stack component to use for navigation
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<ProfileStackNavigatorParams>();
 
 /**
  * Handles navigation for the profile section of the app
