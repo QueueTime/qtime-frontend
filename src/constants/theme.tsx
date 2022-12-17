54f330edd4f8c04f565f7f4872b0868dcc3599f1
Benjamin Kostiuk <benkostiuk1@gmail.com>
2022-12-17 15:06:54 -0500

    Clean up theme constants

--- src/constants/theme.tsx
+++ src/constants/theme.tsx
@@ -1,13 +1,43 @@
+import { StyleSheet } from "react-native";
+
 export const themes = {
+  // Light theme
   light: {
-    name: "light",
-    foreground: "#000000",
-    background: "#ffffff",
+    name: "light", // Name of theme
+    styles: StyleSheet.create({
+      container: {
+        backgroundColor: "#ffffff",
+      },
+      text: {
+        color: "#000000",
+      },
+    }), // Stylesheet with styles associated with theme
+    antDesignOverride: {
+      /*
+        Add any values from the ant default theme you want to override here
+        https://github.com/ant-design/ant-design-mobile-rn/blob/master/components/style/themes/default.tsx
+      */
+      // primary_button_fill: "#34cb38",
+    },
   },
+  // Dark theme
   dark: {
     name: "dark",
-    foreground: "#ffffff",
-    background: "#222222",
+    styles: StyleSheet.create({
+      container: {
+        backgroundColor: "#222222",
+      },
+      text: {
+        color: "#ffffff",
+      },
+    }), // Stylesheet with styles associated with theme
+    antDesignOverride: {
+      /*
+        Add any values from the ant default theme you want to override here
+        https://github.com/ant-design/ant-design-mobile-rn/blob/master/components/style/themes/default.tsx
+      */
+      primary_button_fill: "#a00ceb",
+    },
   },
 };
 
