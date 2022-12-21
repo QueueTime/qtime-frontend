c945847ef6fe503137bb07270e152a78860fa95c
Benjamin Kostiuk <benkostiuk1@gmail.com>
2022-12-21 14:55:37 -0500

    Fix bolding in styled text to use for sign in screen

--- src/components/StyledText.tsx
+++ src/components/StyledText.tsx
@@ -1,16 +1,32 @@
+import React from "react";
 import { StyleSheet, Text } from "react-native";
 
 /**
  * Text wrapper component with global text style applied
  */
-export const StyledText = ({ children, style, ...otherProps }: any) => (
-  <Text {...otherProps} style={[styles.text, style]}>
-    {children}
-  </Text>
-);
+export const StyledText = ({
+  children,
+  style,
+  fontWeight = "normal",
+  ...otherProps
+}: Partial<IStyledTextProps>) => {
+  return (
+    <Text
+      {...otherProps}
+      style={[
+        {
+          fontFamily: fontWeight === "bold" ? "WorkSansBold" : "WorkSans",
+        },
+        style,
+      ]}
+    >
+      {children}
+    </Text>
+  );
+};
 
-const styles = StyleSheet.create({
-  text: {
-    fontFamily: "WorkSans",
-  },
-});
+interface IStyledTextProps {
+  children: React.ReactNode;
+  style: any;
+  fontWeight: "normal" | "bold";
+}
