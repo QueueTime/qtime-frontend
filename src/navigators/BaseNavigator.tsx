2946a626940fccc796437414766b4b18dde961bb
Benjamin Kostiuk <benkostiuk1@gmail.com>
2022-12-20 21:51:41 -0500

    Create base navigator to route bewteen signed in and not

--- src/navigators/BaseNavigator.tsx
+++ src/navigators/BaseNavigator.tsx
@@ -1,7 +1,11 @@
 import { useContext } from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
+<<<<<<< HEAD
 import { AuthContext } from "@contexts/auth";
+=======
+import { AuthContext } from "@contexts/authentication";
+>>>>>>> Create base navigator to route bewteen signed in and not
 import { TabNavigator } from "@navigators/TabNavigator";
 import { SignInScreen } from "@screens/SignInScreen";
 
