b40776c5fd894495b6522a260f785aa2a3ad3164
Benjamin Kostiuk <benkostiuk1@gmail.com>
2022-12-22 16:04:55 -0500

    Add snippet to help with development once we start using the client

---
+++ src/api/client/apis.tsx
@@ -0,0 +1,19 @@
+// **Sample code for creating a client to interact with generated code**
+
+// import "react-native-url-polyfill/auto";
+// import axios from "axios";
+
+// import { PetApi, Configuration } from "../generated";
+
+// const config = new Configuration();
+// const axiosInstance = axios.create({
+//   headers: { Authorization: "YOUR_TOKEN" },
+// });
+
+// const petApi = new PetApi(
+//   config,
+//   "https://petstore3.swagger.io/api/v3",
+//   axiosInstance
+// );
+
+// export { petApi };
