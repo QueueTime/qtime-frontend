0bf58fd1b441109385c285af4b911ba9975815e3
Benjamin Kostiuk <benkostiuk1@gmail.com>
2022-12-26 20:46:45 -0500

    Add icons to bottom navbar

--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -1,4 +1,7 @@
+import { StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
+import { SimpleLineIcons } from "@expo/vector-icons";
 
 import { RewardsScreen } from "@screens/RewardsScreen";
 import { MapScreen } from "@screens/MapScreen";
@@ -9,30 +12,78 @@ import { WaitTimesNavigator } from "@navigators/WaitTimeStackNavigator";
 // Tab component to use for navigation
 const Tab = createBottomTabNavigator();
 
+const ICON_ADJUSTMENT_FACTOR = 5;
+
+const styles = StyleSheet.create({
+  tabBarLabels: {
+    marginBottom: 4,
+    marginTop: -3,
+    fontSize: 11,
+  },
+});
+
 /**
  * Handles tab navigation for the main section of the app
  */
 export const TabNavigator = () => (
-  <Tab.Navigator>
+  <Tab.Navigator screenOptions={{ tabBarLabelStyle: styles.tabBarLabels }}>
     <Tab.Screen
       name={ROUTES.WAIT_TIMES}
       component={WaitTimesNavigator}
-      options={{ headerShown: false }}
+      options={{
+        title: "Wait Times",
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => (
+          <Feather
+            name="clock"
+            size={size - ICON_ADJUSTMENT_FACTOR}
+            color={color}
+          />
+        ),
+      }}
     />
     <Tab.Screen
-      options={{ headerShown: false }}
       name={ROUTES.MAP}
       component={MapScreen}
+      options={{
+        title: "Map",
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => (
+          <Feather
+            name="map-pin" // Alternatively: "navigation"
+            size={size - ICON_ADJUSTMENT_FACTOR}
+            color={color}
+          />
+        ),
+      }}
     />
     <Tab.Screen
-      options={{ title: "Rewards" }}
       name={ROUTES.REWARDS}
       component={RewardsScreen}
+      options={{
+        title: "Rewards",
+        tabBarIcon: ({ color, size }) => (
+          <Feather
+            name="gift"
+            size={size - ICON_ADJUSTMENT_FACTOR}
+            color={color}
+          />
+        ),
+      }}
     />
     <Tab.Screen
       name={ROUTES.PROFILE}
       component={ProfileStackNavigator}
-      options={{ headerShown: false }}
+      options={{
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => (
+          <SimpleLineIcons
+            name="user"
+            size={size - ICON_ADJUSTMENT_FACTOR}
+            color={color}
+          />
+        ),
+      }}
     />
   </Tab.Navigator>
 );
