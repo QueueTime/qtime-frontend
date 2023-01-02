b2f7a37382787fa34e3c5a7dbfbb363bea9ab505
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-01-02 16:47:23 -0500

    Add tests for the app appearance screen

--- src/screens/__tests__/ThemeScreen.test.tsx
+++ src/screens/__tests__/ThemeScreen.test.tsx
@@ -1,12 +1,28 @@
-import { render, screen } from "@testing-library/react-native";
+import { fireEvent, screen } from "@testing-library/react-native";
+
+import { renderWithTheme } from "@contexts/utils/theme.utils";
 import { ThemeScreen } from "@screens/ThemeScreen";
+import { THEME_NAMES } from "@constants/theme";
+
+import "@testing-library/jest-native/extend-expect";
+
+// Test Utils
+const getInfoText = () =>
+  screen.getByText("Customize the app", {
+    exact: false,
+  });
 
 describe("<ThemeScreen />", () => {
-  // Trivial test that checks if the component renders some text on the screen
-  it("has the text app appearance screen", () => {
-    render(<ThemeScreen />);
+  it("renders with the passed theme", () => {
+    renderWithTheme(<ThemeScreen />, THEME_NAMES.dark);
+    expect(getInfoText()).toHaveStyle({ color: "#ffffff" });
+  });
 
-    const textComponent = screen.getByText("App Appearance Screen");
-    expect(textComponent).toBeTruthy();
+  it("updates the theme on radio button selection", () => {
+    renderWithTheme(<ThemeScreen />, THEME_NAMES.light);
+    expect(getInfoText()).toHaveStyle({ color: "#000000" });
+    // Select the dark radio button
+    fireEvent.press(screen.getByText("Dark"));
+    expect(getInfoText()).toHaveStyle({ color: "#ffffff" });
   });
 });
