602b562fad36d9812d15e5617c561d42d4ff9a13
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-01-04 14:53:18 -0500

    Refactor ProfileScreen to match render

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,7 +1,7 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { StyleSheet, ScrollView, Image } from "react-native";
 
-import { Button, WhiteSpace, View, List } from "@ant-design/react-native";
+import { View, List } from "@ant-design/react-native";
 
 import { StyledText } from "@components/StyledText";
 import * as ROUTES from "@constants/routes";
@@ -13,95 +13,107 @@ export const ProfileScreen = ({ navigation }: IProfileScreenProps) => {
   const { user } = useContext(AuthContext);
   const { theme } = useContext(ThemeContext);
 
+  const navigationOptions = [
+    {
+      name: "Suggest a new POI",
+      onPress: () => navigation.navigate(ROUTES.SUGGEST_POI),
+    },
+    {
+      name: "Notification Settings",
+      onPress: () => navigation.navigate(ROUTES.NOTIFICATIONS),
+    },
+    {
+      name: "App Appearance",
+      onPress: () => navigation.navigate(ROUTES.THEME),
+    },
+    {
+      name: "Help",
+      onPress: () => {},
+    },
+    {
+      name: "Delete Account",
+      onPress: () => {},
+    },
+  ];
+
+  const InfoSection = ({ text, subtext }: IInfoSectionProps) =>
+    useMemo(
+      () => (
+        <View style={styles.infoContainer}>
+          <StyledText fontWeight="bold">{text}</StyledText>
+          <StyledText style={theme.styles.subText}>{subtext}</StyledText>
+        </View>
+      ),
+      [text, subtext]
+    );
+
   return (
     <ScrollView style={[theme.styles.screenContainer, styles.container]}>
-      <View style={styles.headerContainer}>
+      <View style={styles.header}>
         <Image
-          source={{
-            uri: user?.photoURL
-              ? user.photoURL
-              : require("@assets/images/GoogleLogo.png"), // Todo need default profile
-          }}
+          source={
+            user?.photoURL
+              ? { uri: user.photoURL }
+              : require("@assets/images/avatar.png")
+          }
           style={styles.profileImg}
         />
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-        >
-          <StyledText fontWeight="bold" style={{ fontSize: 18 }}>
+        <View style={styles.profileInfo}>
+          <StyledText fontWeight="bold" style={styles.name}>
             {user!.displayName}
           </StyledText>
-          <StyledText style={{ fontSize: 16, marginTop: -2 }}>
-            {user!.email}
-          </StyledText>
-          <StyledText style={{ fontSize: 15, color: "grey", marginTop: -4 }}>
-            1271 points
-          </StyledText>
+          <StyledText style={styles.email}>{user!.email}</StyledText>
+          <StyledText style={styles.points}>1271 points</StyledText>
         </View>
       </View>
-      <View style={styles.horizontalLine} />
-      <View style={styles.infoContainer}>
-        <List>
-          <List.Item>Item 1</List.Item>
-          <List.Item>Item 1</List.Item>
-        </List>
-      </View>
-      <View style={styles.horizontalLine} />
+      <List>
+        <List.Item>
+          <InfoSection
+            text="Most frequented POI"
+            subtext="Location 6 • 0.1 km away"
+          />
+        </List.Item>
+        <List.Item>
+          <InfoSection
+            text="Time spent waiting in line"
+            subtext="7 hrs 2 min"
+          />
+        </List.Item>
+      </List>
+      <List
+        styles={{
+          Body: { borderTopWidth: 0 },
+          BodyBottomLine: { borderBottomWidth: 0 },
+        }}
+      >
+        {navigationOptions.map(({ name, onPress }) => (
+          <List.Item
+            key={name}
+            style={styles.navigationOption}
+            arrow="horizontal"
+            onPress={onPress}
+          >
+            <StyledText style={styles.navigationText} fontWeight="bold">
+              {name}
+            </StyledText>
+          </List.Item>
+        ))}
+      </List>
     </ScrollView>
-    // <View style={styles.container}>
-    //   <StyledText style={styles.huge}>Profile Page</StyledText>
-    //   <WhiteSpace />
-    //   <StyledText style={styles.huge}>{`Hi ${user!.displayName}!`}</StyledText>
-    //   <WhiteSpace />
-
-    //   <Button
-    //     type={"primary"}
-    //     onPress={() => navigation.navigate(ROUTES.SUGGEST_POI)}
-    //   >
-    //     <StyledText>Go to POI Suggestion</StyledText>
-    //   </Button>
-    //   <WhiteSpace />
-    //   <Button
-    //     type={"primary"}
-    //     onPress={() => navigation.navigate(ROUTES.NOTIFICATIONS)}
-    //   >
-    //     <StyledText>Go to notifications</StyledText>
-    //   </Button>
-    //   <WhiteSpace />
-    //   <Button
-    //     type={"primary"}
-    //     onPress={() => navigation.navigate(ROUTES.THEME)}
-    //   >
-    //     <StyledText>Go to appearance settings</StyledText>
-    //   </Button>
-    // </View>
   );
 };
 
 const styles = StyleSheet.create({
-  huge: {
-    fontSize: 20,
-  },
   container: {
-    //backgroundColor: "red",
-    padding: 0,
+    padding: 0, // Override the padding on screen container to work with horizontal lines
   },
-  headerContainer: {
+  header: {
     flex: 1,
     flexDirection: "row",
     justifyContent: "center",
-    //backgroundColor: "green",
     paddingTop: 10,
-    paddingLeft: 25,
-    paddingRight: 25,
+    paddingHorizontal: 25,
     paddingBottom: 30,
-  },
-  infoContainer: {
-    //backgroundColor: "lightblue",
-    padding: 25,
   },
   profileImg: {
     resizeMode: "cover",
@@ -111,12 +123,31 @@ const styles = StyleSheet.create({
     borderRadius: 999,
     marginRight: 20,
   },
-  horizontalLine: {
-    borderBottomColor: "grey",
-    borderBottomWidth: StyleSheet.hairlineWidth,
+  profileInfo: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  name: { fontSize: 18 },
+  email: { fontSize: 16, marginTop: -2 },
+  points: { fontSize: 15, color: "grey", marginTop: -4 },
+  infoContainer: {
+    padding: 15,
+  },
+  navigationOption: {
+    paddingLeft: 25,
+    paddingRight: 10,
+  },
+  navigationText: {
+    paddingVertical: 8,
   },
 });
 
 interface IProfileScreenProps {
   navigation: ProfileScreenProps["navigation"];
+}
+
+interface IInfoSectionProps {
+  text: string;
+  subtext: string;
 }
