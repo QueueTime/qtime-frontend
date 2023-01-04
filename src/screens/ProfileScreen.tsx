c42a1f02a3a179650edcad28cbe0f80a0528c909
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-01-04 11:59:54 -0500

    Commit working changes for restart

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,49 +1,83 @@
 import { useContext } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, ScrollView, Image } from "react-native";
 
-import { Button, WhiteSpace } from "@ant-design/react-native";
+import { Button, WhiteSpace, View, List } from "@ant-design/react-native";
 
 import { StyledText } from "@components/StyledText";
 import * as ROUTES from "@constants/routes";
 import { ProfileScreenProps } from "@navigators/ProfileStackNavigator";
 import { AuthContext } from "@contexts/auth";
+import { ThemeContext } from "@contexts/theme";
 
 export const ProfileScreen = ({ navigation }: IProfileScreenProps) => {
   const { user } = useContext(AuthContext);
-
-  // TODO: Figure out better solution for lacking a user
-  if (!user) {
-    return null;
-  }
+  const { theme } = useContext(ThemeContext);
 
   return (
-    <View style={styles.container}>
-      <StyledText style={styles.huge}>Profile Page</StyledText>
-      <WhiteSpace />
-      <StyledText style={styles.huge}>{`Hi ${user.displayName}!`}</StyledText>
-      <WhiteSpace />
+    <ScrollView style={[theme.styles.screenContainer, styles.container]}>
+      <View style={styles.headerContainer}>
+        <Image
+          source={{
+            uri: user?.photoURL
+              ? user.photoURL
+              : require("@assets/images/GoogleLogo.png"), // Todo need default profile
+          }}
+          style={styles.profileImg}
+        />
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <StyledText fontWeight="bold" style={{ fontSize: 18 }}>
+            {user!.displayName}
+          </StyledText>
+          <StyledText style={{ fontSize: 16, marginTop: -2 }}>
+            {user!.email}
+          </StyledText>
+          <StyledText style={{ fontSize: 15, color: "grey", marginTop: -4 }}>
+            1271 points
+          </StyledText>
+        </View>
+      </View>
+      <View style={styles.horizontalLine} />
+      <View style={styles.infoContainer}>
+        <List>
+          <List.Item>Item 1</List.Item>
+          <List.Item>Item 1</List.Item>
+        </List>
+      </View>
+      <View style={styles.horizontalLine} />
+    </ScrollView>
+    // <View style={styles.container}>
+    //   <StyledText style={styles.huge}>Profile Page</StyledText>
+    //   <WhiteSpace />
+    //   <StyledText style={styles.huge}>{`Hi ${user!.displayName}!`}</StyledText>
+    //   <WhiteSpace />
 
-      <Button
-        type={"primary"}
-        onPress={() => navigation.navigate(ROUTES.SUGGEST_POI)}
-      >
-        <StyledText>Go to POI Suggestion</StyledText>
-      </Button>
-      <WhiteSpace />
-      <Button
-        type={"primary"}
-        onPress={() => navigation.navigate(ROUTES.NOTIFICATIONS)}
-      >
-        <StyledText>Go to notifications</StyledText>
-      </Button>
-      <WhiteSpace />
-      <Button
-        type={"primary"}
-        onPress={() => navigation.navigate(ROUTES.THEME)}
-      >
-        <StyledText>Go to appearance settings</StyledText>
-      </Button>
-    </View>
+    //   <Button
+    //     type={"primary"}
+    //     onPress={() => navigation.navigate(ROUTES.SUGGEST_POI)}
+    //   >
+    //     <StyledText>Go to POI Suggestion</StyledText>
+    //   </Button>
+    //   <WhiteSpace />
+    //   <Button
+    //     type={"primary"}
+    //     onPress={() => navigation.navigate(ROUTES.NOTIFICATIONS)}
+    //   >
+    //     <StyledText>Go to notifications</StyledText>
+    //   </Button>
+    //   <WhiteSpace />
+    //   <Button
+    //     type={"primary"}
+    //     onPress={() => navigation.navigate(ROUTES.THEME)}
+    //   >
+    //     <StyledText>Go to appearance settings</StyledText>
+    //   </Button>
+    // </View>
   );
 };
 
@@ -52,10 +86,34 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
   container: {
+    //backgroundColor: "red",
+    padding: 0,
+  },
+  headerContainer: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    flexDirection: "row",
     justifyContent: "center",
+    //backgroundColor: "green",
+    paddingTop: 10,
+    paddingLeft: 25,
+    paddingRight: 25,
+    paddingBottom: 30,
+  },
+  infoContainer: {
+    //backgroundColor: "lightblue",
+    padding: 25,
+  },
+  profileImg: {
+    resizeMode: "cover",
+    overflow: "hidden",
+    width: 100,
+    height: 100,
+    borderRadius: 999,
+    marginRight: 20,
+  },
+  horizontalLine: {
+    borderBottomColor: "grey",
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
 
