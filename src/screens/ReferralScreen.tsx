8364438a90483c3fd1b23f2066d25d14e735136b
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-01-05 13:32:31 -0500

    Update referral screen with icons and error codes

--- src/screens/ReferralScreen.tsx
+++ src/screens/ReferralScreen.tsx
@@ -1,76 +1,149 @@
 import { useContext, useState } from "react";
-import { Image, StyleSheet, TouchableOpacity } from "react-native";
+import { Image, StyleSheet, TouchableOpacity, Dimensions } from "react-native";
 
-import { Button, View, WhiteSpace, InputItem } from "@ant-design/react-native";
+import { View, WhiteSpace, InputItem } from "@ant-design/react-native";
+import ConfettiCannon from "react-native-confetti-cannon";
+
 import { StyledText } from "@components/StyledText";
 import { ThemeContext } from "@contexts/theme";
 
-export const ReferralScreen = () => {
+const MAX_CHARS = 6;
+const PLACEHOLDER = "XCJDHC";
+const CODE = "ABCDEF";
+
+export const ReferralScreen = ({
+  onReferralComplete,
+}: IReferralScreenProps) => {
   const { theme } = useContext(ThemeContext);
-  const [referralCode, setReferralCode] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [hasError, setHasError] = useState(false);
+  const [errorCode, setErrorCode] = useState("");
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  const cannonColors = ["#1677ff", "#5c5c5c", "#16adff"];
+  const inputTextStyle = {
+    color: hasError ? "red" : theme.styles.text.color,
+  };
+
+  const onSubmit = () => {
+    if (isTransitioning) {
+      return; // Prevent multiple submissions once one is validated
+    }
+
+    if (userInput.length < MAX_CHARS) {
+      setHasError(true);
+      setErrorCode(`Code must be ${MAX_CHARS} characters long.`);
+      return;
+    }
+
+    // TODO: Replace this with a call to check if the code is valid
+    if (userInput !== CODE) {
+      setHasError(true);
+      setErrorCode("Invalid referral code. Try again.");
+      return;
+    }
+
+    setIsTransitioning(true);
+    // TODO: Add code to attribute points
+    setTimeout(() => {
+      setIsTransitioning(false);
+      onReferralComplete();
+    }, 3000);
+  };
 
   const onInput = (value: string) => {
-    console.log(value);
-    setReferralCode(value);
+    setUserInput(value);
+    setHasError(false);
   };
 
   const onNoReferralCodePress = () => {
-    console.log("Next page");
+    onReferralComplete();
   };
 
   return (
-    <View style={[theme.styles.screenContainer, { paddingBottom: 35 }]}>
-      <View style={styles.container}>
-        <Image
-          style={styles.image}
-          source={require("@assets/images/RewardBox.png")}
+    <View style={[theme.styles.screenContainer, styles.container]}>
+      <Image
+        style={styles.image}
+        source={require("@assets/images/RewardBox.png")}
+      />
+      <WhiteSpace />
+      <View style={styles.inputContainer}>
+        <InputItem
+          style={inputTextStyle}
+          autoCapitalize="characters"
+          textAlignVertical="center"
+          textAlign="center"
+          value={userInput}
+          placeholder={PLACEHOLDER}
+          placeholderTextColor={theme.styles.text.color}
+          cursorColor={theme.styles.text.color}
+          maxLength={MAX_CHARS}
+          onChange={(value) => onInput(value)}
+          onSubmitEditing={onSubmit}
+          returnKeyType="done"
         />
-        <WhiteSpace />
-        <View style={{ width: "80%", height: 40 }}>
-          <InputItem
-            autoCapitalize="characters"
-            textAlignVertical="center"
-            textAlign="center"
-            value={referralCode}
-            placeholder="XCJ3HC"
-            maxLength={6}
-            onChange={onInput}
-          />
-        </View>
-        <WhiteSpace size="xl" />
-        <StyledText style={[theme.styles.primaryColor, styles.title]}>
-          Have a referral code?
-        </StyledText>
-        <WhiteSpace size="sm" />
-        <StyledText>Enter your referral code to earn bonus points!</StyledText>
-        <WhiteSpace size="xl" />
-        <TouchableOpacity onPress={onNoReferralCodePress}>
-          <StyledText style={theme.styles.primaryColor}>
-            I don't have one
-          </StyledText>
-        </TouchableOpacity>
       </View>
-
-      {/* <Button type="primary">Submit</Button> */}
+      <View style={styles.errorWrapper}>
+        {hasError && (
+          <StyledText style={styles.errorText}>{errorCode}</StyledText>
+        )}
+      </View>
+      <StyledText
+        fontWeight="bold"
+        style={[theme.styles.primaryColor, styles.title]}
+      >
+        Have a referral code?
+      </StyledText>
+      <WhiteSpace size="sm" />
+      <StyledText>Enter your referral code to earn bonus points!</StyledText>
+      <WhiteSpace size="xl" />
+      <TouchableOpacity
+        disabled={isTransitioning}
+        onPress={onNoReferralCodePress}
+      >
+        <StyledText style={theme.styles.primaryColor}>
+          I don't have one
+        </StyledText>
+      </TouchableOpacity>
+      {isTransitioning && (
+        <ConfettiCannon
+          count={80}
+          origin={{ x: Dimensions.get("window").width / 2, y: -15 }}
+          fadeOut
+          colors={cannonColors}
+        />
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  image: {
-    resizeMode: "contain",
-    width: 200,
-    height: 200,
-    marginBottom: 5,
-  },
-  title: {
-    fontSize: 21,
-    marginTop: 6,
-  },
   container: {
     flex: 1,
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
   },
+  image: {
+    resizeMode: "contain",
+    width: 200,
+    height: 200,
+    marginBottom: 5,
+  },
+  inputContainer: { width: "80%", height: 40 },
+  title: {
+    fontSize: 21,
+    marginTop: 6,
+  },
+  errorWrapper: {
+    marginTop: 4,
+    height: 20,
+  },
+  errorText: {
+    color: "red",
+  },
 });
+
+interface IReferralScreenProps {
+  onReferralComplete: () => void;
+}
