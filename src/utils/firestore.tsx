14013f565977a2a64a97dc9d9733fd51ff3b0285
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-01-12 22:04:19 -0500

    Add utils for firestore and errors

--- src/utils/firestore.tsx
+++ src/utils/firestore.tsx
@@ -1,17 +1,31 @@
-import { USER_COLLECTION } from "@constants/firestore";
 import firestore, {
   FirebaseFirestoreTypes,
 } from "@react-native-firebase/firestore";
 
 /**
+ * Firestore constants
+ */
+
+const USER_COLLECTION = "users";
+const DEFAULT_USER = {
+  hasCompletedOnboarding: false,
+};
+
+/**
+ * Create a new user document with their email as their id and default values
+ * @param userEmail Email for the new user
+ */
+export const createUser = async (userEmail: string) =>
+  firestore().collection(USER_COLLECTION).doc(userEmail).set(DEFAULT_USER);
+
+/**
  * Update a user document to indicate they have completed onboarding
  * @param userEmail User email of user to update
  */
-export const completeUserOnboarding = async (userEmail: string) => {
-  await firestore().collection(USER_COLLECTION).doc(userEmail).update({
+export const completeUserOnboarding = async (userEmail: string) =>
+  firestore().collection(USER_COLLECTION).doc(userEmail).update({
     hasCompletedOnboarding: true,
   });
-};
 
 /**
  * Subscribe to updates about a document in the user collection
@@ -28,5 +42,9 @@ export const subscribeToUserUpdates = (
     .doc(userEmail)
     .onSnapshot((doc) => onSnapshot(doc));
 
+/**
+ * Delete a user document from the user collection
+ * @param userEmail User email of user to delete
+ */
 export const deleteUser = (userEmail: string) =>
   firestore().collection(USER_COLLECTION).doc(userEmail).delete();
