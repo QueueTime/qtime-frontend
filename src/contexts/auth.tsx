b3a7bf00f8074892f67ef1fc72df1a2b66d5ea00
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-01-12 22:00:02 -0500

    Add userProfile state that pulls from firestore

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -3,6 +3,9 @@ import React, { useState, useEffect, SetStateAction, Dispatch } from "react";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 
+import { createUser, subscribeToUserUpdates } from "@utils/firestore";
+import { displayError } from "@utils/error";
+
 const AUTH_WEB_CLIENT_ID =
   "476284740655-hn9ftfe6at300fpgmn3i8282i3nignnj.apps.googleusercontent.com";
 
@@ -36,10 +39,12 @@ const signOut = async () => {
 
 export const AuthContext = React.createContext<{
   user: FirebaseAuthTypes.User | null;
+  userProfile: IUserProfile | null;
   signIn: () => Promise<FirebaseAuthTypes.UserCredential>;
   signOut: () => Promise<void>;
 }>({
   user: null,
+  userProfile: null,
   signIn,
   signOut,
 });
@@ -48,7 +53,8 @@ export const AuthContext = React.createContext<{
  * Authentication context provider.
  * Exposes the current `user`, `signIn` and `signOut` properties.
  *
- * `user` is the authenticated user of the app. Null if no user is authenticated.
+ * `user` is the authenticated Google user of the app. Null if no user is authenticated.
+ * `userProfile` is the user profile data for the authenticated user pulled from firebase.
  * `signIn()` signs in the current user using Google sign in.
  * `signOut()` signs out the current user. Will subsequently set the user to null.
  *
@@ -80,9 +86,10 @@ export const AuthProvider = ({
   setFirebaseInitializing,
 }: IAuthProviderProps) => {
   const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+  const [userProfile, setUserProfile] = useState<IUserProfile | null>(null);
 
   useEffect(() => {
-    const subscriber = auth().onAuthStateChanged((userState) => {
+    const subscriber = auth().onAuthStateChanged(async (userState) => {
       setUser(userState);
       if (isFirebaseInitializing) setFirebaseInitializing(false);
     });
@@ -90,8 +97,33 @@ export const AuthProvider = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    if (!user?.email) {
+      setUserProfile(null);
+      return;
+    }
+    // Subscribe to updates about the user and update userProfile
+    const subscriber = subscribeToUserUpdates(user.email, async (doc) => {
+      const data = doc.data();
+      if (data) {
+        setUserProfile({
+          email: doc.id,
+          hasCompletedOnboarding: data.hasCompletedOnboarding,
+        });
+      } else {
+        // Create a new user if one doesn't already exist to subscribe to
+        try {
+          await createUser(user.email!);
+        } catch (error) {
+          displayError(`Cannot create new account. Try again later. ${error}`);
+        }
+      }
+    });
+    return () => subscriber(); // unsubscribe on unmount
+  }, [user?.email]);
+
   return (
-    <AuthContext.Provider value={{ user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user, userProfile, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
@@ -101,4 +133,9 @@ interface IAuthProviderProps {
   children: React.ReactNode;
   isFirebaseInitializing: boolean;
   setFirebaseInitializing: Dispatch<SetStateAction<boolean>>;
+}
+
+interface IUserProfile {
+  email: string;
+  hasCompletedOnboarding: boolean;
 }
