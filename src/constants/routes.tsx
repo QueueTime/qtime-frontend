bdd4b49be21fbca8c287bf151ee50fe5f7e0a8e9
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-01-12 21:35:48 -0500

    Add routes for onboarding

--- src/constants/routes.tsx
+++ src/constants/routes.tsx
@@ -1,9 +1,6 @@
-/* Route names */
-export const SIGN_UP = "SignUp";
-export const REFERRAL = "Referral";
-export const TERMS_OF_SERVICE = "TermsOfService";
-export const ONBOARDING = "Onboarding";
-
+/*
+ Route names 
+*/
 export const WAIT_TIMES = "WaitTimes";
 // This is required for the wait time stack navigator
 // This route corresponds to the home for the wait times tab
@@ -20,3 +17,8 @@ export const PROFILE_HOME = "ProfileHome";
 export const SUGGEST_POI = "SuggestPOI";
 export const NOTIFICATIONS = "Notifications";
 export const THEME = "Theme";
+
+export const SIGN_UP_FLOW = "SignUp";
+export const TERMS_OF_SERVICE = "TermsOfService";
+export const REFERRAL = "Referral";
+export const ONBOARDING = "Onboarding";
