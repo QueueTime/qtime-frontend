bace30416709cf5e71baa4a770cea362dfa21903
Rahil Shah <31329775+rahilllshah@users.noreply.github.com>
2023-01-26 12:37:00 -0500

    Update styles.tsx

--- src/constants/styles.tsx
+++ src/constants/styles.tsx
@@ -2,6 +2,7 @@ export const PRIMARY_COLOR_LIGHT = "#1677ff";
 export const WEAK_COLOR_LIGHT = "#999999";
 export const LIGHT_COLOR_LIGHT = "#CCCCCC";
 export const BACKGROUND_COLOR_LIGHT = "#F5F5F5";
+export const BORDER_COLOR_LIGHT = "#EEEEEE";
 export const WHITE_COLOR_LIGHT = "#FFFFFF";
 export const BLACK_COLOR_LIGHT = "#000000";
 
@@ -9,6 +10,7 @@ export const BLACK_COLOR_LIGHT = "#000000";
 export const PRIMARY_COLOR_DARK = "#1677ff";
 export const WEAK_COLOR_DARK = "#999999";
 export const LIGHT_COLOR_DARK = "#CCCCCC";
-export const BACKGROUND_COLOR_DARK = "#333333";
+export const BACKGROUND_COLOR_DARK = "#333333
+export const BORDER_COLOR_LIGHT = "#333333";
 export const WHITE_COLOR_DARK = "#FFFFFF";
 export const BLACK_COLOR_DARK = "#000000";
