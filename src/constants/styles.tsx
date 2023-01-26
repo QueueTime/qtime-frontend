812e4ded312d2da2f4745547e4608a00c2485d5c
Rahil Shah <31329775+rahilllshah@users.noreply.github.com>
2023-01-26 12:23:31 -0500

    Update with constants

--- src/constants/styles.tsx
+++ src/constants/styles.tsx
@@ -1,2 +1,14 @@
 export const PRIMARY_COLOR_LIGHT = "#1677ff";
-export const PRIMARY_COLOR_DARK = "#a020f0";
+export const WEAK_COLOR_LIGHT = "#999999";
+export const LIGHT_COLOR_LIGHT = "#CCCCCC";
+export const BACKGROUND_COLOR_LIGHT = "#F5F5F5";
+export const WHITE_COLOR_LIGHT = "#FFFFFF";
+export const BLACK_COLOR_LIGHT = "#000000";
+
+
+export const PRIMARY_COLOR_DARK = "#1677ff";
+export const WEAK_COLOR_DARK = "#999999";
+export const LIGHT_COLOR_DARK = "#CCCCCC";
+export const BACKGROUND_COLOR_DARK = "#333333";
+export const WHITE_COLOR_DARK = "#FFFFFF";
+export const BLACK_COLOR_DARK = "#000000";
