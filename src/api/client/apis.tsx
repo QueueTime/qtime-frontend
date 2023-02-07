562c0acf3acf2f2f298eecf49c5d8d3b961a28db
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-02-06 21:42:05 -0500

    Update api with generated code and clients

--- src/api/client/apis.tsx
+++ src/api/client/apis.tsx
@@ -1,19 +1,23 @@
 // **Sample code for creating a client to interact with generated code**
 
-// import "react-native-url-polyfill/auto";
-// import axios from "axios";
+import "react-native-url-polyfill/auto";
+import axios from "axios";
+// @ts-ignore
+import { BASE_URL } from "@env";
 
-// import { PetApi, Configuration } from "../generated";
+import { UserApi, Configuration, DefaultApi, POIApi } from "../generated";
 
-// const config = new Configuration();
-// const axiosInstance = axios.create({
-//   headers: { Authorization: "YOUR_TOKEN" },
-// });
+const config = new Configuration();
+const axiosInstance = axios.create({
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+const baseUrl = BASE_URL;
 
-// const petApi = new PetApi(
-//   config,
-//   "https://petstore3.swagger.io/api/v3",
-//   axiosInstance
-// );
+const userApi = new UserApi(config, baseUrl, axiosInstance);
+const poiApi = new POIApi(config, baseUrl, axiosInstance);
+const defaultApi = new DefaultApi(config, baseUrl, axiosInstance);
 
-// export { petApi };
+export { userApi, poiApi, defaultApi };
