4937e9c60c97682e35d161be1959883de253e814
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-03-05 14:11:48 -0500

    Add defaults to node-dotenv to fix issues

--- babel.config.js
+++ babel.config.js
@@ -22,7 +22,16 @@ module.exports = function (api) {
       [
         "module:react-native-dotenv",
         {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+          blocklist: null,
+          allowlist: null,
+          blacklist: null, // DEPRECATED
+          whitelist: null, // DEPRECATED
+          safe: false,
           allowUndefined: false,
+          verbose: false,
         },
       ],
     ],
